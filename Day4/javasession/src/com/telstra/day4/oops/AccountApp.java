a38d316d8345411036ef481740b59c6bf4516b35
Mahadik <AniketAshok.Mahadik@team.telstra.com>
2023-07-27 14:53:13 +0530

    day4 addition

---
+++ Day4/javasession/src/com/telstra/day4/oops/AccountApp.java
@@ -0,0 +1,51 @@
+package com.telstra.day4.oops;
+
+class Account
+{
+	int accountId;
+	String acountHolderName;
+	double balance;
+	
+	Account(int accountId,String acountHolderName,double balance){
+		this.accountId = accountId;
+		this.acountHolderName = acountHolderName;
+		this.balance = balance;
+//		System.out.println("constructer new");
+	}
+//	@Override
+//	public String toString() {
+//		return "Account [acountId=" + acountId + ", acountHolderName=" + acountHolderName + ", balance=" + balance
+//				+ "]";
+//	}
+	void displayAccountDetails() {
+		System.out.println("account id " + accountId);
+		System.out.println("name " + acountHolderName);
+		System.out.println("balance " + balance);
+	}
+	
+	void deposit(int amountToDeposit) {
+		balance += amountToDeposit;
+//		System.out.println("new balance " + balance);
+	}
+	
+	void withdraw(int amountToWithdraw) {
+		balance -= amountToWithdraw;
+//		System.out.println("new balance " + balance);
+	}
+	double getBalance() {
+		System.out.println("New balance is : " + balance);
+		return balance;
+	}
+}
+
+public class AccountApp{
+	public static void main(String[] args) {
+		Account account = new Account(1001,"Aniket",5000);
+		account.displayAccountDetails();
+		account.deposit(1000);
+		account.getBalance();
+		account.withdraw(2000);
+		account.getBalance();
+	}
+}
+
